2c09233a528ef0fbcb3f4c14c53849d2fc267a46
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-09-16 22:09:46 +0700

    hw1 - exe005 - p1.27 - done

---
+++ src/hw1_21000699_dangngocquan/exercise005/p1dot27/Main.java
@@ -0,0 +1,8 @@
+package hw1_21000699_dangngocquan.exercise005.p1dot27;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.start();
+    }
+}
