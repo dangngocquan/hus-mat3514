864bcc35aafec68b57eca0f5c0eb0e16a469fce5
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-09-20 00:43:54 +0700

    hw2 - exe004 - task041 - Fix

--- src/hw2_21000699_dangngocquan/exercise004/task041/Main.java
+++ src/hw2_21000699_dangngocquan/exercise004/task041/Main.java
@@ -6,51 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] a = new int[n];
+        Integer[] a = new Integer[n];
         for (int i = 0; i < n; i++) a[i] = scanner.nextInt();
         scanner.close();
 
-        MergeSort.sort(a);
+        BubbleSort.sort(a);
 
         for (int e : a) System.out.print(e + " ");
     }
 
-    private static class MergeSort {
-        public static void sort(int[] a) {
-            mergeSort(a, 0, a.length-1);
-        }
-
-        // Sort a[left, right]
-        private static void mergeSort(int[] a, int left, int right) {
-            if (left < right) {
-                int mid = (left + right) / 2;
-                mergeSort(a, left, mid);
-                mergeSort(a, mid + 1, right);
-                merge(a, left, mid, right);
-            }
-        }
-
-        // merge two sorted sub-array a[left, mid] and a[mid+1, right]
-        private static void merge(int[] a, int left, int mid, int right) {
-            int bLength = right - left + 1;
-            int[] b = new int[bLength];
-            int i = 0;
-            int i1 = left;
-            int i2 = mid+1;
-            while (i1 <= mid && i2 <= right) {
-                if (a[i1] <= a[i2]) {
-                    b[i] = a[i1];
-                    i1++;
-                } else {
-                    b[i] = a[i2];
-                    i2++;
+    private static class BubbleSort {
+        public static <T extends Comparable<T>> void sort(T[] a) {
+            for (int i = 0; i < a.length-1; i++) {
+                boolean isSorted = true;
+                for (int j = 1; j < a.length-i; j++) {
+                    if (a[j-1].compareTo(a[j]) > 0) {
+                        isSorted = false;
+                        T temp = a[j-1];
+                        a[j-1] = a[j];
+                        a[j] = temp;
+                    }
                 }
-                i++;
+                if (isSorted) break;
             }
-            while (i1 <= mid) b[i++] = a[i1++];
-            while (i2 <= right) b[i++] = a[i2++];
-            i = 0;
-            while (i < bLength) a[i+left] = b[i++];
         }
     }
 }
