417440f3245590e5fc10c04c00d69373ae95d558
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-09-23 13:13:53 +0700

    Review hw1 - 21000697 - lethinhung

--- src/hw1_21000699_dangngocquan/exercise005/p1dot27/Main.java
+++
@@ -1,8 +0,0 @@
-package hw1_21000699_dangngocquan.exercise005.p1dot27;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.start();
-    }
-}
--- src/hw1_21000699_dangngocquan/exercise006/p2dot32/Main.java
+++
@@ -1,8 +0,0 @@
-package hw1_21000699_dangngocquan.exercise006.p2dot32;
-
-public class Main {
-    public static void main(String[] args) {
-        new Config();
-        new App();
-    }
-}
