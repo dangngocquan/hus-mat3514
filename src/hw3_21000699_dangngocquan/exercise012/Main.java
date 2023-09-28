1bb6df23bfe0e3d9ce3f4774390dc328814d0966
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-09-28 18:12:02 +0700

    hw3 - exe012 - dne

---
+++ src/hw3_21000699_dangngocquan/exercise012/Main.java
@@ -0,0 +1,27 @@
+package hw3_21000699_dangngocquan.exercise012;
+
+public class Main {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode currentNode = head;
+            ListNode previousNode = null;
+            while (currentNode != null) {
+                if (previousNode != null && previousNode.val == currentNode.val) {
+                    previousNode.next = currentNode.next;
+                } else {
+                    previousNode = currentNode;
+                }
+                currentNode = currentNode.next;
+            }
+            return head;
+        }
+    }
+}
