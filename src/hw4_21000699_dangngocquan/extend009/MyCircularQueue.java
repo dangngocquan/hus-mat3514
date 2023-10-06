f127bc780fd0f3692f7e3730504f851b6f537407
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-10-07 02:31:35 +0700

    hw4 - extend009 - done

--- src/hw4_21000699_dangngocquan/extend009/MyCircularQueue.java
+++ src/hw4_21000699_dangngocquan/extend009/MyCircularQueue.java
@@ -1,2 +1,43 @@
-package hw4_21000699_dangngocquan.extend009;public class MyCircularQueue {
+package hw4_21000699_dangngocquan.extend009;
+
+class MyCircularQueue {
+    private int[] data;
+    private int size;
+    private int firstIndex;
+
+    public MyCircularQueue(int k) {
+        data = new int[k];
+    }
+
+    public boolean enQueue(int value) {
+        if (size == data.length) return false;
+        data[(firstIndex + size) % data.length] = value;
+        size++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (size == 0) return false;
+        firstIndex = (firstIndex + 1) % data.length;
+        size--;
+        return true;
+    }
+
+    public int Front() {
+        if (size == 0) return -1;
+        return data[firstIndex];
+    }
+
+    public int Rear() {
+        if (size == 0) return -1;
+        return data[(firstIndex + size - 1) % data.length];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == data.length;
+    }
 }
