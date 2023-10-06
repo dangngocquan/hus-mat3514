109c7fb5c818ca9ce71c039a2a26ba589438939f
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-10-07 00:53:37 +0700

    hw4 - extend002 - done

---
+++ src/hw4_21000699_dangngocquan/extend002/a/MyStack.java
@@ -0,0 +1,23 @@
+package hw4_21000699_dangngocquan.extend002.a;
+
+class MyStack {
+    int top;
+    int arr[] = new int[1000];
+
+    MyStack() {
+        top = -1;
+    }
+
+    //Function to push an integer into the stack.
+    void push(int a) {
+        // Your code here
+        arr[++top] = a;
+    }
+
+    //Function to remove an item from top of the stack.
+    int pop() {
+        // Your code here
+        if (top == -1) return -1;
+        return arr[top--];
+    }
+}
---
+++ src/hw4_21000699_dangngocquan/extend002/b/MyStack.java
@@ -0,0 +1,32 @@
+package hw4_21000699_dangngocquan.extend002.b;
+
+class MyStack {
+    class StackNode {
+        int data;
+        StackNode next;
+
+        StackNode(int a) {
+            data = a;
+            next = null;
+        }
+    }
+
+    StackNode top;
+
+    //Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        StackNode node = new StackNode(a);
+        node.next = top;
+        top = node;
+    }
+
+    //Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        if (top == null) return -1;
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+}
