fcc7a340dfb62fdb17513a3aabe7235715a290e6
Đặng Ngọc Quân <dangngocquan104@gmail.com>
2023-10-20 01:09:24 +0700

    hw5 - doing

--- src/hw5_21000699_dangngocquan/extend011/InorderTraversal.java
+++ src/hw5_21000699_dangngocquan/extend011/InorderTraversal.java
@@ -1,5 +1,7 @@
 package hw5_21000699_dangngocquan.extend011;
 
+import java.util.ArrayList;
+
 public class InorderTraversal {
     static class Node {
         int data;
@@ -11,10 +13,25 @@ public class InorderTraversal {
         }
     }
 
-//    static class Solution {
-//        // Function to return a list containing the inorder traversal of the tree.
-//        ArrayList<Integer> inOrder(Node root) {
-//            // Code
-//        }
-//    }
+    static class Solution {
+        // Function to return a list containing the inorder traversal of the tree.
+        ArrayList<Integer> inOrder(Node root) {
+            // Code
+            ArrayList<Integer> list = new ArrayList<>();
+            if (root != null) {
+                list.addAll(inOrder(root.left));
+                list.add(root.data);
+                list.addAll(inOrder(root.right));
+            }
+            return list;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        System.out.println(new Solution().inOrder(root));
+        // [2, 1, 3]
+    }
 }
---
+++ src/hw5_21000699_dangngocquan/extend020/MakeBinaryTreeFromLinkedList.java
@@ -0,0 +1,73 @@
+package hw5_21000699_dangngocquan.extend020;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MakeBinaryTreeFromLinkedList {
+    static class Tree {
+        int data;
+        Tree left;
+        Tree right;
+
+        Tree(int d) {
+            data = d;
+            left = null;
+            right = null;
+        }
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    static class GfG {
+        // Function to make binary tree from linked list.
+        public static Tree convert(Node head, Tree node) {
+            // add code here.}
+            if (node == null) node = new Tree(head.data);
+            node.data = head.data;
+            Tree root = node;
+            head = head.next;
+            Queue<Tree> queue = new LinkedList<>();
+            int index = 1;
+            while (head != null) {
+                if (index % 2 == 1) {
+                    node.left = new Tree(head.data);
+                    queue.add(node.left);
+                } else {
+                    node.right = new Tree(head.data);
+                    queue.add(node.right);
+                    node = queue.poll();
+                }
+                head = head.next;
+                index++;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        Tree tree = GfG.convert(node1, null);
+        System.out.println(tree.data); // 1
+        System.out.println(tree.left.data); // 2
+        System.out.println(tree.right.data); // 3
+        System.out.println(tree.left.left.data); // 4
+        System.out.println(tree.left.right.data); // 5
+    }
+}
